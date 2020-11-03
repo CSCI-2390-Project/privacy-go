5b30840a12f92b8d49b269a0d0a8b6ac6787d6fe
Akshat Mahajan <akshatm.bkk@gmail.com>
2020-11-02 23:58:24 -0500

    Complete `isActionAllowed` function and include tests
    This commit:
    
    1. Reorganizes file names - all public methods now go into
    `public_methods.go`, all private methods not related to policies goes
    into `private_methods.go`, and all policy-related functions in
    `policies.go`. Files used in test cases are now in `testing`.
    
    2. Finishes implementing the `isActionAllowed` function. This currently
    only supports iterating through the current call stack, but will pass on
    all the desired testcases.
    
    3. Adds tests to `policies_test.go` that test the behaviour of
    `isActionAllowed`.
    
    4. Added an example implementation of specifying function *ordering* as
    opposed to just function names. This allows a lot of specificity and
    control for the end user.
    
    5. Implement loading in the policies file completely. By default, this
    only loads in the policy file once on library import. It turns out to be
    too complicated to refresh policy files after an interval, and honestly
    its not actually all that important to have.
    
    6. (Proposed) Drops support for allowing conditions to pass if message
    fields have a certain decrypted attribute. The reason this is dropped is
    because none of our publicly exported methods `PermissionedDecrypt`, etc.
    actually have access to the *entire* message - just to the field we want
    to encrypt. I'm also worried it'll just add room for weird conflicts
    where an attribute doesn't want to be decrypted inside a function but we
    decrypt it anyway just to check on its internals.
    
    Another reason why I no longer think it's worth the effort to implement
    this is because it's probably not going to be all that relevant if we
    support function order whitelisting (as this commit implements). A user
    can accomplish the same effect as `message.foo = X` for attribute `bar` by:
    
    - Adding a policy rule `{allowed: true, if: "allowed_function"}` for
    attribute `foo`
    
    - Adding a policy rule `{allowed:true, if:
    "function_called_only_if_foo_passes < allowed_function"}` for
    attribute `bar`
    
    - Writing code that checks if `message.GetFoo() == X` in
    `allowed_function` and only then
    doing `message.GetBar()` inside `function_called_only_if_foo_passes`
    immediately after.
    
    Basically, if the use case is supporting conditional processing, then
    you can use function order whitelisting for the exact same effect and
    with more security overall.

--- policies_test.go
+++ policies_test.go
@@ -1,11 +1,25 @@
 package privacy
 
 import (
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"os"
 	"reflect"
 	"testing"
 )
 
+func setupExamplePolicy(t *testing.T, filepath string) func(t *testing.T) {
+	t.Logf("Setting %s", environment_variable)
+	os.Setenv(environment_variable, filepath)
+	refreshPolicies()
+	return func(t *testing.T) {
+		t.Logf("Unsetting %s", environment_variable)
+		os.Unsetenv(environment_variable)
+		refreshPolicies()
+	}
+}
+
 func TestParsing(t *testing.T) {
+
 	policies, err := loadPolicies("testing/files/example.json")
 	if err != nil {
 		t.Errorf("Encountered error opening the file %+v", err)
@@ -13,7 +27,7 @@ func TestParsing(t *testing.T) {
 
 	expected := map[string]Conditions{
 		"HelloRequest.name": Conditions{
-			CopyConditions:   &[]ConditionStatement{{Key: "message.name", Value: "Rodriguez"}},
+			CopyConditions:   &[]ConditionStatement{{Allowed: true, If: "HelloRequest.GetName < main.main"}},
 			PrintConditions:  &[]ConditionStatement{},
 			ModifyConditions: nil,
 		},
@@ -31,6 +45,156 @@ func TestParsing(t *testing.T) {
 			}
 		} else {
 			t.Errorf("Expected %s to be in policies %+v", expected_key, policies)
+		}
+	}
+}
+
+func TestIsMatchesPositionIndependent(t *testing.T) {
+
+	if !matches("me", []string{"me"}) {
+		t.Errorf("Expected `me` to match `[me]`")
+	}
+
+	if !matches("me", []string{"you", "me"}) {
+		t.Errorf("Expected `me` to match `[you, me]`")
+	}
+
+	if !matches("you", []string{"me", "you"}) {
+		t.Errorf("Expected `you` to match `[me, you]`")
+	}
+
+	if matches("us", []string{"me", "you"}) {
+		t.Errorf("Did not expect `us` to match `[me, you]`")
+	}
+
+	if matches("me < us", []string{"me", "you"}) {
+		t.Errorf("Did not expect `me < us` to match `[me, you]`")
+	}
+
+	if !matches("me < us", []string{"me", "us", "you"}) {
+		t.Errorf("Expected `me < us` to match `[me, us, you]`")
+	}
+
+	if !matches("me < us < you", []string{"me", "us", "you"}) {
+		t.Errorf("Expected `me < us < you` to match `[me, us, you]`")
+	}
+
+	if !matches("me < you", []string{"me", "us", "you"}) {
+		t.Errorf("Expected `me < you` to match `[me, us, you]`")
+	}
+
+	if !matches("me < you", []string{"me", "us", "them", "you"}) {
+		t.Errorf("Expected `me < you` to match `[me, us,, them, you]`")
+	}
+
+	if matches("me < you", []string{"you", "us", "them", "me"}) {
+		t.Errorf("Did not expect `me < you` to match `[you, us, them, me]`")
+	}
+
+	if matches("", []string{"me", "us", "you"}) {
+		t.Errorf("Did not expected `us` to match `[me, us, you]`")
+	}
+
+	if !matches("me", []string{"me", "me", "me"}) {
+		t.Errorf("Expected `me` to match `[me, me, me]`")
+	}
+
+	if matches("me < me < me < me", []string{"me", "me", "me"}) {
+		t.Errorf("Did not expect `me < me < me < me` to match `[me, me, me]`")
+	}
+
+	if !matches("me < us", []string{"me", "you", "you", "us"}) {
+		t.Errorf("Expected `me < us` to match `[me, you, you, us]`")
+	}
+}
+
+func TestIsActionAllowedFunction(t *testing.T) {
+	teardownFunc := setupExamplePolicy(t, "testing/files/example.json")
+	defer teardownFunc(t)
+
+	cases := []struct {
+		message    protoreflect.Name
+		field      protoreflect.Name
+		act        action
+		trace      []string
+		shouldPass bool
+	}{{
+		"HelloRequest",
+		"name",
+		copying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest",
+		"name",
+		printing,
+		[]string{"HelloRequest.GetName", "main.main"},
+		false,
+	}, {
+		"HelloRequest",
+		"name",
+		modifying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest",
+		"name",
+		copying,
+		[]string{"main.main"},
+		false,
+	}, {
+		"HelloRequest",
+		"name",
+		printing,
+		[]string{"main.main"},
+		false,
+	}, {
+		"HelloRequest",
+		"name",
+		modifying,
+		[]string{"main.main"},
+		true,
+	}, {
+		"HelloRequest",
+		"foo",
+		copying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest",
+		"foo",
+		printing,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest",
+		"foo",
+		modifying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest2",
+		"name",
+		copying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest2",
+		"name",
+		printing,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}, {
+		"HelloRequest2",
+		"name",
+		modifying,
+		[]string{"HelloRequest.GetName", "main.main"},
+		true,
+	}}
+
+	for _, caseof := range cases {
+		if isActionAllowed(caseof.message, caseof.field, caseof.act, caseof.trace) != caseof.shouldPass {
+			t.Errorf("Expected case %v to pass, but did not", caseof)
 		}
 	}
 }
